c62ea2c52f68b5bb5e9e50dd3f20debba4604af0
VCLdesigns <Bart.Victor.Loots@gmail.com>
2022-11-06 14:36:34 +0100

    added audio

--- js/index.js
+++ js/index.js
@@ -3,28 +3,31 @@ var deur = document.querySelector("#deur");
 var sleutel = document.querySelector("#sleutel");
 var reset = document.querySelector("#reset");
 var help = document.querySelector("#help");
+var krijs = new Audio("audio/krijs.mp3");
 var Gevonden = false;
+
 help.addEventListener("click", toonHint);
 function toonHint(evt) {
     if (Gevonden == false) {
-        infobalk.innerHTML = "Een sleutel zou nu wel handig zijn."
+        infobalk.innerHTML = "Hopelijk hebben ze een sleutel klaargelegd."
     } else {
-        infobalk.innerHTML = "Waar zou ik die sleutel voor kunnen gebruiken?"
+        infobalk.innerHTML = "Ik heb de sleutel gevonden, tijd om ze te verrassen."
     }
 }
 sleutel.addEventListener("click", sleutelGevonden);
 function sleutelGevonden(evt) {
-    infobalk.innerHTML = "Kijk, een sleutel gevonden.";
+    infobalk.innerHTML = "één sleutel... check!";
     Gevonden = true;
     sleutel.classList.add("sleutel_gevonden");
 }
 deur.addEventListener("click", deurOpen);
 function deurOpen(evt) {
     if (Gevonden == false) {
-        infobalk.innerHTML = "De deur is op slot... ";
+        infobalk.innerHTML = "Geen reactie op mijn geklop en de deur is op slot... ";
     } else {
-        infobalk.innerHTML = "Zombies!";
+        infobalk.innerHTML = "Damn. What the hell... Run away!";
         deur.style.transform = "rotateY(130deg)";
+        krijs.play();
         help.classList.toggle("reset_en_help_weg");
         reset.classList.toggle("reset_en_help_weg");
     }
@@ -32,7 +35,7 @@ function deurOpen(evt) {
 reset.addEventListener("click", resetAlles);
 function resetAlles(evt) {
     Gevonden = false;
-    infobalk.innerHTML = "Déjà Vu?!";
+    infobalk.innerHTML = "wow... Déjà Vu?!";
     sleutel.classList.remove("sleutel_gevonden");
     var topLocatie = 10 + Math.random() * (600 - 10);
     sleutel.style.top = topLocatie + "px";
